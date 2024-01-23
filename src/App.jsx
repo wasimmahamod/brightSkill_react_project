661356ab8699ee6f61dd11f82282882783f7e3ac
wasimmahamod <wasimmahamodraza@gmail.com>
2024-01-23 15:35:28 +0600

    serviceDone

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import Banner from "./components/Banner";
+import Service from "./components/Service";
 
 const App = () => {
   return (
-    <div >App</div>
-  )
-}
+    <>
+      <Navbar />
+      <Banner />
+      <Service />
+    </>
+  );
+};
 
-export default App
+export default App;
